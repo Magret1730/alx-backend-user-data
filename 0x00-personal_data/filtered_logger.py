c50847299d9ee5575b148a5fa1ea9a25758a4f81
Magret <belloabiodun17@gmail.com>
2024-07-04 15:00:58 +0000

    Resolved task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -70,7 +70,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Database connection using environment variables
     """
