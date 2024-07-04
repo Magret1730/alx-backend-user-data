02ee0694f38633bbf167de91a81de1236da01200
Magret <belloabiodun17@gmail.com>
2024-07-04 13:40:33 +0000

    Task 2 solution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Task 0 and 1"""
+"""
+Definition of filter_datum function that returns an obfuscated log message
+"""
 import logging
 import re
 from typing import List
@@ -7,23 +9,60 @@ from typing import List
 
 def filter_datum(fields: str, redaction: str, message: str, separator: str) ->\
                                                                         str:
-    """function that that returns the log message obfuscated"""
+    """
+    Function that that returns the log message obfuscated
+    Args:
+    fields: (list): list of strings indicating fields to obfuscate
+    redaction (str): what the field will be obfuscated to
+    message (str): the log line to obfuscate
+    separator (str): the character separating the fields
+    """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: str):
+        """
+        Class initialization
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return: formatted string
+        """
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Function that creates and configures the logger
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler = streamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addhandler(stream_handler)
+
+    return logger
+
+
+PII_FIELDS = ("name", "email", "phone", "password", "ip")
