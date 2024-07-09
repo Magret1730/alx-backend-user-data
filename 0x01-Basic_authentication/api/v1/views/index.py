ab6239c0147b17eb4664c0f1ef116f5b1d7d56c5
Magret <belloabiodun17@gmail.com>
2024-07-09 12:43:16 +0000

    Deleted SimpleAPI folder

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,25 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+
+    Return:
+        - unauthorized error status
+    """
+    return abort(401, description = "Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+
+    Return:
+        - forbidden error status
+    """
+    return abort(403, description = "forbidden")
