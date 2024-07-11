d3ea070226f5f8b37167f13012fb789b7421d726
Magret <belloabiodun17@gmail.com>
2024-07-11 12:11:56 +0000

    Task 6 solution

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Session Authentication Module """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -50,3 +51,25 @@ class SessionAuth(Auth):
         user_id = self.user_id_by_session_id.get(session_id)
 
         return user_id
+
+    def current_user(self, request=None):
+        """
+        Method that returns a User instance based on cookie value
+
+        Args:
+            request (Request): The Flask request object.
+
+        Returns:
+            User: The user instance if the session ID is valid, else None.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        print(f"Session_id:   {session_id}")
+        user_id = self.user_id_by_session_id.get(session_id)
+        if user_id is None:
+            return None
+        print(f"User_id:   {user_id}")
+        user = User.get(user_id)
+        print(f"User:  {user}")
+        return user
